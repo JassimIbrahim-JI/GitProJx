e3876c3896269cdd608bb080fd028e5f3f940fa1
TOSHIBA <jassim.ibrhm@outlook.com>
2022-09-25 17:51:58 +0300

    Create user followers and followings list

---
+++ app/src/main/java/com/gitpro/gitidea/adapters/UserAdapter.java
@@ -0,0 +1,229 @@
+package com.gitpro.gitidea.adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.fragment.app.FragmentActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.gitpro.gitidea.customs.CustomTextView;
+import com.gitpro.gitidea.R;
+import com.gitpro.gitidea.fragments.ProfileFragment;
+import com.gitpro.gitidea.models.Notification;
+import com.gitpro.gitidea.models.User;
+import com.gitpro.gitidea.ui.ExploreActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder> implements Filterable {
+
+    List<User>uList;
+    List<User>userFilters;
+    Context context;
+    FirebaseUser firebaseUser;
+    private static FirebaseFirestore db;
+    private boolean isFragment;
+
+
+    public UserAdapter(List<User> uList, Context context,boolean isFragment) {
+        this.uList = uList;
+        this.context = context;
+        userFilters=uList;
+        this.isFragment=isFragment;
+    }
+
+    @NonNull
+    @Override
+    public UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        firebaseUser= FirebaseAuth.getInstance().getCurrentUser();
+        db=FirebaseFirestore.getInstance();
+        return new UserViewHolder(LayoutInflater.from(context).inflate(R.layout.user_item,parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserViewHolder holder, int position) {
+         final User user= uList.get(position);
+         holder.addUser.setVisibility(View.VISIBLE);
+         holder.name.setText(user.userName);
+         holder.email.setText(user.email);
+        Picasso.get().load(user.photoUrl)
+                .noPlaceholder().into(holder.imageUrl);
+           isFollowing(user.userId,holder.addUser);
+        if (user.userId.equals(firebaseUser.getUid())){
+            holder.addUser.setVisibility(View.GONE);
+        }
+
+        holder.addUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String btnStatus=holder.addUser.getText().toString();
+                if (btnStatus.equals("follow+")){
+                    db.collection("users/"+firebaseUser.getUid()+"/followings")
+                            .document(user.userId)
+                            .get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                if (!task.getResult().exists()) {
+                                    Map<String, Object> setValues = new HashMap<>();
+                                    setValues.put("date", FieldValue.serverTimestamp());
+                                    db.collection("users/" + firebaseUser.getUid() + "/followings")
+                                            .document(user.userId)
+                                            .set(setValues);
+                                }
+                            }
+                    });
+
+                    addNotification(user.userId,user.photoUrl);
+
+                    db.collection("users/" + user.userId + "/followers")
+                            .document(firebaseUser.getUid())
+                            .get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                            if (!task.getResult().exists()) {
+                                Map<String, Object> setValues = new HashMap<>();
+                                setValues.put("date", FieldValue.serverTimestamp());
+                                db.collection("users/" + user.userId + "/followers")
+                                        .document(firebaseUser.getUid())
+                                        .set(setValues);
+                            }
+                        }
+                    });
+
+                }
+                else {
+                        db.collection("users/" + firebaseUser.getUid() + "/followings")
+                                .document(user.userId)
+                                .delete();
+                        db.collection("users/" + user.userId + "/followers")
+                                .document(firebaseUser.getUid())
+                                .delete();
+                }
+            }
+        });
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isFragment) {
+                    SharedPreferences.Editor editor = context.getSharedPreferences("PROFILE", Context.MODE_PRIVATE)
+                            .edit();
+                    editor.putString("profileId", user.userId);
+                    editor.apply();
+                    ((FragmentActivity) context).getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ProfileFragment())
+                            .commitNow();
+                }
+                else {
+                    Intent intent = new Intent(context, ExploreActivity.class);
+                    Bundle bundle=new Bundle();
+                    bundle.putString("publisherId", user.userId);
+                    intent.putExtras(bundle);
+                    context.startActivity(intent);
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return uList.size();
+    }
+
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+                FilterResults results=new FilterResults();
+                if (charSequence.length()==0 && charSequence.toString().isEmpty()){
+                    results.values=userFilters;
+                    results.count= userFilters.size();
+                }
+                else {
+                    String usrSearch=String.valueOf(charSequence).toLowerCase();
+                    List<User>users=new ArrayList<>();
+                    for (User user:userFilters){
+                        if (user.userName.contains(usrSearch.toLowerCase())||user.email.contains(usrSearch.toLowerCase())){
+                            users.add(user);
+                        }
+                    }
+                    results.values=users;
+                    results.count=users.size();
+                }
+
+                return results;
+            }
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                   uList=(List<User>)filterResults.values;
+                   notifyDataSetChanged();
+            }
+        };
+    }
+
+    public static class UserViewHolder extends RecyclerView.ViewHolder{
+        private CircleImageView imageUrl;
+        private CustomTextView name,email;
+        AppCompatButton addUser;
+        public UserViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            imageUrl=itemView.findViewById(R.id.photo_search);
+            name=itemView.findViewById(R.id.user_search);
+            email=itemView.findViewById(R.id.email_search);
+            addUser=itemView.findViewById(R.id.follow_search);
+
+        }
+    }
+    private void isFollowing(final String userId, AppCompatButton compatButton) {
+        DocumentReference userRef = db.collection("users/" + firebaseUser.getUid() + "/followings")
+                .document(userId);
+        userRef.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                if (error==null){
+                    if (value!=null && value.exists()){
+                        compatButton.setText("following");
+                    }
+                    else {
+                        compatButton.setText("follow+");
+                    }
+                }
+            }
+        });
+    }
+    private void addNotification(String userId,String photoUrl) {
+        DocumentReference notificationRef=db.collection("notifications")
+                .document(userId);
+        Notification notification=new Notification(firebaseUser.getUid(),photoUrl,"started following you..",
+                "","",false,false);
+
+        notificationRef.set(notification);
+    }
+}
---
+++ app/src/main/java/com/gitpro/gitidea/ui/UserFollowersActivity.java
@@ -0,0 +1,130 @@
+package com.gitpro.gitidea.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.gitpro.gitidea.R;
+import com.gitpro.gitidea.adapters.UserAdapter;
+import com.gitpro.gitidea.models.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserFollowersActivity extends AppCompatActivity {
+
+   private UserAdapter userAdapter;
+    private List<User> mUsers;
+    private FirebaseFirestore db;
+    RecyclerView recyclerView;
+    List<String>idList;
+    String id,title;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_followers);
+
+        db=FirebaseFirestore.getInstance();
+        Intent intent = getIntent();
+        id = intent.getStringExtra("userId");
+        title = intent.getStringExtra("tag");
+
+        Toolbar toolbar = findViewById(R.id.followers_tb);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(title);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        recyclerView = findViewById(R.id.followers_rv);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mUsers = new ArrayList<>();
+        userAdapter = new UserAdapter( mUsers, this,false);
+        recyclerView.setAdapter(userAdapter);
+
+        idList = new ArrayList<>();
+
+        switch (title) {
+            case "followerPage" :
+                getFollowers();
+                break;
+
+            case "followingPage":
+                getFollowings();
+                break;
+
+        }
+    }
+
+    private void getFollowers() {
+
+        db.collection("users/"+id+"/followers")
+                .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                idList.clear();
+                for (QueryDocumentSnapshot snapshot:task.getResult()){
+                    idList.add(snapshot.getId());
+                }
+                showUsers();
+            }
+        });
+        
+    }
+
+    private void getFollowings() {
+
+        db.collection("users/"+id+"/followings")
+                .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                  idList.clear();
+                  for (QueryDocumentSnapshot snapshot:task.getResult()){
+                      idList.add(snapshot.getId());
+                  }
+                  showUsers();
+            }
+        });
+    
+    }
+
+    private void showUsers() {
+        db.collection("users")
+                .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+           mUsers.clear();
+           for (QueryDocumentSnapshot snapshot:task.getResult()){
+               User user=snapshot.toObject(User.class);
+               for (String id:idList){
+                   if (user.userId.equals(id)) {
+                       mUsers.add(user);
+                   }
+               }
+           }
+                Log.d("list Followers", mUsers.toString());
+                userAdapter.notifyDataSetChanged();
+            }
+        });
+    }
+
+}
